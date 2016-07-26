54e6533ac07f982cc6811f5ac76f59bce88e029d
Tim Groeneveld <tim@timg.ws>
2016-07-26 10:44:22 +0800

    Welcome to `rowtool`.

---
+++ README.md
@@ -0,0 +1,111 @@
+# jQuery rowTool
+
+### Dynamically add rows to a table from a Qty. input
+
+Here's the job: you need to dymically add a whole bunch of rows to a table, based on a qty. input box.
+You don't want to write the same 100 lines of code all the time.
+ 
+Here's the solution: use this plugin!
+
+This plugin will automatically ensure that the rows are added and removed from the table.
+
+Limits can be placed (`min` and `max` values on the qty. input box)
+
+You can add rows on the server side, this plugin will not clobber them.
+
+## Usage
+
+1. Include jQuery:
+
+	```html
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	```
+
+2. Include plugin's code:
+
+	```html
+	<script src="dist/jquery.rowtool.min.js"></script>
+	```
+
+3. Write the HTML:
+
+    ```html
+    <p>How many things do you want?</p>
+    <input id="service-qty" name="service-qty" type="number" value="0" min="0" max="99" aria-valuemin="1">
+    
+     
+    <!-- build a template for the rows... -->
+    <script type="html/service-template" name="service-template">
+        <tr class="service">
+            <td>#<span class="service-qty-number"></span></td>
+            <td><input type="text" name="service[][nickname]" placeholder="Nicky"></td>
+            <td><input type="text" name="service[][firstname]" placeholder="Nicholas"></td>
+            <td><input type="text" name="service[][lastname]" placeholder="Jonas"></td>
+        </tr>
+    </script>
+     
+    <!-- and add a table that we will modify! -->
+    <table class="services">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nick name</th>
+            <th>First name</th>
+            <th>Last name</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
+    ```
+
+4. Call the plugin:
+
+	```javascript
+    $(".services").rowTool({
+        // The template for the row.
+        template: $('script[type="html/service-template"][name="service-template"]'),
+       
+        // This is the name of the input box(es) that we want to modify
+        groupName: 'service',
+       
+        // The input boxes are called this...
+        inputBoxes: ['nickname', 'firstname', 'lastname'],
+       
+        // This is the element with the input box that has the qty.
+        // When it changes, we want to update the table with this # of rows
+        qtyInput: $("#service-qty"),
+       
+        // [Optional] This is the class that has the # of the row.
+        qtyRow: ".service-qty-number",
+    });
+	```
+
+## Structure
+
+The basic structure of the project is given in the following way:
+
+```
+├── demo/
+│   └── index.html
+├── dist/
+│   ├── jquery.rowtool.js
+│   └── jquery.rowtool.min.js
+├── src/
+│   └── jquery.rowtool.js
+├── .editorconfig
+├── .gitignore
+├── .jshintrc
+├── .travis.yml
+├── Gruntfile.js
+└── package.json
+```
+
+#### [demo/](https://github.com/vonex-labs/jquery-rowtool/tree/master/demo)
+
+Contains a simple HTML file to demonstrate the plugin.
+
+## License
+
+Software is licensed under the `MIT License`.
---
+++ src/jquery.rowtool.js
@@ -0,0 +1,174 @@
+/* jshint quotmark: false, unused: false */
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;(function ($, window, document, undefined) {
+
+    "use strict";
+
+    // undefined is used here as the undefined global variable in ECMAScript 3 is
+    // mutable (ie. it can be changed by someone else). undefined isn't really being
+    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+    // can no longer be modified.
+
+    // window and document are passed through as local variables rather than global
+    // as this (slightly) quickens the resolution process and can be more efficiently
+    // minified (especially when both are regularly referenced in your plugin).
+
+    // Create the defaults once
+    var pluginName = "rowTool",
+        defaults = {
+            qtyInput: null,
+            limitQty: null,
+            minQty: null,
+            maxQty: null,
+            hideOnNone: null,
+            template: null,
+            htmlTemplate: null,
+            updateTemplate: null,
+            inputBoxes: [],
+            groupName: null,
+        };
+
+    // The actual plugin constructor
+    function Plugin(element, options) {
+        this.element = element;
+
+        // jQuery has an extend method which merges the contents of two or
+        // more objects, storing the result in the first object. The first object
+        // is generally empty as we don't want to alter the default options for
+        // future instances of the plugin
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+
+    // Avoid Plugin.prototype conflicts
+    $.extend(Plugin.prototype, {
+        init: function () {
+            // Make sure that a template has been defined.
+            if (this.settings.template === null) {
+                $.error('You need to define a template for new rows.');
+            }
+
+            // Set up the HTML template
+            this.setupHtmlTemplate(this, this.settings.template);
+
+            // Has the plugin been set to limit qty's manually, or will we pull the figures from the qty attr?
+            if (this.settings.limitQty === null) {
+                this.settings.limitQty = this.willLimitQty(this);
+            }
+
+            this.setupQtyLimit(this);
+            //this.setupTable( "jQuery Boilerplate" );
+        },
+        setupHtmlTemplate: function (that, template) {
+            if ($(template).length === 1) {
+                var $rowTemplate = $($(template).html());
+                that.settings.htmlTemplate = $rowTemplate;
+            }
+        },
+        willLimitQty: function (that) {
+            // Check if the qty input box has limits (min/max) attributes.
+            var minQty = $(that.settings.qtyInput).attr("min");
+            var maxQty = $(that.settings.qtyInput).attr("max");
+
+            if (!(minQty === undefined && maxQty === undefined)) {
+                that.settings.minQty = minQty;
+                that.settings.maxQty = maxQty;
+
+                $(that.element).data("numberOfLines", $(that.element).find("tbody").find("tr").length);
+
+                return true;
+            }
+
+            return false;
+        },
+        setupQtyLimit: function (that) {
+            if (that.settings.limitQty === true) {
+                $(that.settings.qtyInput).change(function () {
+                    var value = parseInt($(this).val());
+
+                    var min = that.settings.minQty;
+                    var max = that.settings.maxQty;
+
+                    if (value < min) {
+                        value = min;
+                        $(this).val(min);
+                    } else if (value > max) {
+                        value = max;
+                        $(this).val(max);
+                    }
+
+                    that.updateNumberOfLinesInTable(that, value);
+                });
+            }
+        },
+        updateNumberOfLinesInTable: function (that, newQty) {
+            var $infoTable = $(that.element);
+            var $lines = $infoTable.data("numberOfLines");
+
+            if ($lines < newQty) {
+                var $lineTemplate = that.settings.htmlTemplate.clone();
+
+                for (var i = $lines + 1; i < newQty + 1; i++) {
+                    var $newLine = that.makeFromTemplate(that, $lineTemplate, i);
+
+                    $infoTable.find("tbody").append($newLine);
+
+                    if (that.settings.updateTemplate !== null) {
+                        $newLine = that.settings.updateTemplate.call(that.element, $newLine);
+                    }
+                }
+            } else {
+                for (var x = newQty + 1; x < $lines + 1; x++) {
+                    $infoTable.find("tbody tr[data-row-number=\"" + x + "\"]").remove();
+                }
+            }
+
+            $infoTable.data("numberOfLines", newQty);
+        },
+        makeFromTemplate: function(that, $line, $number) {
+            $line = $line.clone();
+
+            if (that.settings.groupName !== null) {
+                var selectBoxes = that.settings.inputBoxes;
+
+                $line.attr('class', 'isdn');
+                $line.attr("data-row-number", $number);
+
+                var $lineNumber;
+                if (($lineNumber = that.settings.qtyRow) !== null) {
+                    $line.find($lineNumber).html($number);
+                }
+
+                selectBoxes.forEach(function (element, index) {
+                    var templateName = 'name="' + that.settings.groupName + '[][' + element + ']"';
+                    var correctName = that.settings.groupName + '[' + $number + '][' + element + ']';
+
+                    var $input = $line.find('input[' + templateName + ']');
+                    $line.find('input[' + templateName + ']').attr('name', correctName);
+                    //$input.mask($input.data('mask'));
+                });
+            }
+
+            return $line;
+        },
+        setupTable: function (text) {
+            // some logic
+            $(this.element).text(text);
+        }
+    });
+
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" +
+                    pluginName, new Plugin(this, options));
+            }
+        });
+    };
+
+})(jQuery, window, document);
